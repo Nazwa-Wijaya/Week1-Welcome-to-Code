e42fa510abb9c4c5f7395951483cbda7133a2346
Nazwa-Wijaya <nazwawijaya89@gmail.com>
2025-07-23 16:33:49 +0700

    push part3

---
+++ quiz/part3/soal5.js
@@ -0,0 +1,16 @@
+// Problem buatlah bintang seperti berikut
+let input = 5
+//hasilnya
+//*
+//**
+//***
+//****
+//*****
+
+for(let i = 0; i <= input; i++){
+    let bintang = '';
+    for(let j = 1; j <= i; j++){
+        bintang += '*'
+    }
+    console.log(bintang)
+}
